cbda52db8ebfb80745e7e67733b816b78e719918
Azuny <crippyua@gmail.com>
2016-11-06 11:43:42 +0200

    Add duplicate control to write_songs

--- pandora-likes-parser.py
+++ pandora-likes-parser.py
@@ -1,11 +1,12 @@
 # This Python file uses the following encoding: utf-8
 from bs4 import BeautifulSoup
 
-file = "./pandora.html"
+file = "./pandora1.html"
 files = []
 
 
 def read_file(f):
+    """Convert html to beautiful soup instance."""
     with open(f, mode="r") as html_doc:
         soup = BeautifulSoup(html_doc, "html.parser")
     return soup
@@ -20,32 +21,25 @@ def write_songs(soup):
         title = line.h3.a.text
         station = line.find("a", "like_context_stationname").text.encode(
             "utf-8")
-        try:
-            song = str(artist + " - " + title).strip()
-        except UnicodeEncodeError as e:
-            song = u" - ".join((artist, title)).encode("utf-8").strip()
+        song = u" - ".join((artist, title)).encode("utf-8").strip()
         if station not in stations:
             stations[station] = [song]
         else:
             stations[station].append(song)
 
     with open("./songs.txt", mode="a+") as f:
+        file = f.read()
         songs_set = set()
         for st in stations:
             for s in stations.get(st):
-                song_line = s + " | " + st + '\n'
-                if s not in songs_set:
+                song_line = ''.join((s, " | ", st, "\n"))
+                if s not in songs_set and s not in file:
                     songs_set.add(s)
                     f.write(song_line)
+                    print("++ " + song_line[:-1])
                 else:
-                    print s
+                    print("-- " + s + ' is already on the list')
     return
 
 
-write_songs(read_file(file))
-# with open("./songs.txt", "r") as f1, open("./songs10.txt", "w+") as f2:
-#     songs = set()
-#     for line in f1.readlines():
-#         songs.add(line)
-#     for song in songs:
-#         f2.write(song)
+# write_songs(read_file(file))
